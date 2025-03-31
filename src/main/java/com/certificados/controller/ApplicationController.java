8a6124ed3f3a2dec381de4e71d5c8285990edd7f
AlejandroParuma <113801583+AlejandroParuma@users.noreply.github.com>
2025-03-30 19:23:00 -0500

    second commit

--- src/main/java/com/certificados/controller/ApplicationController.java
+++ src/main/java/com/certificados/controller/ApplicationController.java
@@ -2,6 +2,8 @@ package com.certificados.controller;
 
 import com.certificados.model.ConsultaDocumentoRequest;
 import com.certificados.model.EnvioDocumentoRequest;
+import com.certificados.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -14,20 +16,24 @@ import java.util.Map;
 @RequestMapping("/api")
 public class ApplicationController {
 
+    @Autowired
+    private ApplicationService ftpService;
+
     @GetMapping
-    public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("WELCOME TO FULL STACK JAVA DEVELOPER, PUNE");
+    public ResponseEntity<String> getOpen(){
+        return ResponseEntity.ok("Working");
     }
 
     @PostMapping("/consultar")
     public Map<String, Integer> consultarCertificados(@RequestBody ConsultaDocumentoRequest consultaRequest) throws IOException {
+        int count = ftpService.getDocumentCount(consultaRequest.getAnio(), consultaRequest.getNit());
 
-        return Collections.singletonMap("count", 1);
+        return Collections.singletonMap("count", count);
     }
 
     @PostMapping("/enviar")
     public ResponseEntity<String> enviarCertificados(@RequestBody EnvioDocumentoRequest envioRequest) throws IOException {
-
+        ftpService.enviarCorreo(envioRequest.getAnio(), envioRequest.getNit(), envioRequest.getNombre(), envioRequest.getEmail());
         return new ResponseEntity<>("Se enviaron los certificados al correo: " + envioRequest.getEmail(), HttpStatus.OK);
     }
 
